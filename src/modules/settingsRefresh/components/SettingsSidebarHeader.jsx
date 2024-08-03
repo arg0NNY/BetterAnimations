67b91a1f901bdfed69d9346571eb351d1d104a7e
Oleg Kapranov <arg0nny@mail.ru>
2024-08-03 22:08:38 +0300

    Settings header redesign

--- src/modules/settingsRefresh/components/SettingsSidebarHeader.jsx
+++ src/modules/settingsRefresh/components/SettingsSidebarHeader.jsx
@@ -1,10 +1,12 @@
 import IconBrand from '@/components/icons/IconBrand'
 import { css } from '@/modules/Style'
+import { Common } from '@/modules/DiscordModules'
 
 function SettingsSidebarHeader () {
   return (
     <div className="BA__settingsSidebarHeader">
       <IconBrand />
+      <Common.Text variant="text-md/semibold">etterAnimations</Common.Text>
     </div>
   )
 }
@@ -13,13 +15,21 @@ export default SettingsSidebarHeader
 
 css
 `.BA__settingsSidebarHeader {
+    position: relative;
     margin-top: -30px;
-    padding-bottom: 2px;
+    padding-bottom: 10px;
 }
     
 .BA__settingsSidebarHeader > svg {
     display: block;
-    width: 64px;
-    height: 64px;
+    width: 52px;
+    height: 52px;
+}
+
+.BA__settingsSidebarHeader > div {
+    position: absolute;
+    top: 26px;
+    left: 41px;
+    color: var(--header-primary);
 }`
 `SettingsSidebarHeader`
