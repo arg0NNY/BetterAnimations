712dcb53b2fe645f83ce333d7588f6415d15cea6
Oleg Kapranov <arg0nny@mail.ru>
2024-10-28 20:26:06 +0300

    fix: refactor forceAppUpdate

--- src/helpers/forceUpdate.js
+++ src/helpers/forceUpdate.js
@@ -1,10 +1,6 @@
-import { LocaleStore, Dispatcher, updateTheme, ThemeStore } from '@/modules/DiscordModules'
+import { Dispatcher } from '@/modules/DiscordModules'
 
 export function forceAppUpdate () {
-  const locale = LocaleStore.getLocale()
-  Dispatcher.dispatch({ type: 'I18N_LOAD_START', locale })
-  setTimeout(() => {
-    Dispatcher.dispatch({ type: 'I18N_LOAD_SUCCESS', locale })
-    updateTheme(ThemeStore.theme)
-  })
+  Dispatcher.dispatch({ type: 'DOMAIN_MIGRATION_START' })
+  setTimeout(() => Dispatcher.dispatch({ type: 'DOMAIN_MIGRATION_SKIP' }))
 }
