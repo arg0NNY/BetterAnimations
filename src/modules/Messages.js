aecc57720a6b8cb6de6b68944dda8ff3cd27364a
Oleg Kapranov <arg0nny@mail.ru>
2025-03-15 21:07:08 +0300

    feat: provide messages for PackSchema regex

--- src/modules/Messages.js
+++ src/modules/Messages.js
@@ -2,7 +2,8 @@
 const Messages = {
   SELECT_ANIMATION_TO_ENABLE: 'Select animation to enable',
   FORCE_DISABLED_BY_ANIMATION: animationName => `Force disabled by selected animation (${animationName})`,
-  HEAVY_MODULE_ALERT: moduleName => `${moduleName} animations are heavy on performance, may stutter and slow down app navigation.`
+  HEAVY_MODULE_ALERT: moduleName => `${moduleName} animations are heavy on performance, may stutter and slow down app navigation.`,
+  SHOULD_BE_VALID_URL: 'Should be a valid URL'
 }
 
 export default Messages
--- src/modules/animation/schemas/PackSchema.js
+++ src/modules/animation/schemas/PackSchema.js
@@ -1,17 +1,18 @@
 import { z } from 'zod'
 import regex from '@/helpers/regex'
 import AnimationSchema from '@/modules/animation/schemas/AnimationSchema'
+import Messages from '@/modules/Messages'
 
 const PackSchema = z.object({
   name: z.string().trim().min(2).max(255),
   author: z.string().trim().min(1),
-  version: z.string().regex(regex.semver),
+  version: z.string().regex(regex.semver, 'Should match semver format'),
   description: z.string().optional(),
-  invite: z.string().regex(regex.url).optional(),
-  authorLink: z.string().regex(regex.url).optional(),
-  donate: z.string().regex(regex.url).optional(),
-  patreon: z.string().regex(regex.url).optional(),
-  website: z.string().regex(regex.url).optional(),
+  invite: z.string().regex(regex.url, Messages.SHOULD_BE_VALID_URL).optional(),
+  authorLink: z.string().regex(regex.url, Messages.SHOULD_BE_VALID_URL).optional(),
+  donate: z.string().regex(regex.url, Messages.SHOULD_BE_VALID_URL).optional(),
+  patreon: z.string().regex(regex.url, Messages.SHOULD_BE_VALID_URL).optional(),
+  website: z.string().regex(regex.url, Messages.SHOULD_BE_VALID_URL).optional(),
 
   animations: AnimationSchema.array()
 })
