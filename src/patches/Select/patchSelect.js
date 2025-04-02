2c270c8df45beaf60952972980fd5213f401c695
Oleg Kapranov <arg0nny@mail.ru>
2025-04-02 22:00:16 +0300

    fix(selects): Discord bug updating the position 1 frame too late

--- src/patches/Select/patchSelect.js
+++ src/patches/Select/patchSelect.js
@@ -1,26 +1,50 @@
 import { Patcher } from '@/BdApi'
-import { Common, Select, SingleSelect } from '@/modules/DiscordModules'
+import { SearchableSelect, Select, SingleSelect } from '@/modules/DiscordModules'
 import Position from '@/enums/Position'
 import findInReactTree from '@/utils/findInReactTree'
+import { useLayoutEffect, useState } from 'react'
 
-function patchSearchableSelect () {
-  Patcher.after(Common.SearchableSelect, 'render', (self, args, value) => {
+function createPatcher () {
+  let patchedPopout = null
+
+  return (self, args, value) => {
     const popout = findInReactTree(value, m => m?.renderPopout)
-    if (popout) popout.align = Position.Center
-  })
+    if (!popout) return
+
+    popout.align = Position.Center
+
+    Patcher.after(popout, 'renderPopout', (self, args, value) => {
+      // Patching like this to avoid infinite updates
+      if (!patchedPopout) {
+        const { type: original } = value
+        patchedPopout = props => {
+          const value = original(props)
+
+          // Fix Discord bug where they messed up and used `useEffect` instead of `useLayoutEffect`
+          // to update popout position after measuring the height
+          const [v, setV] = useState(0)
+          useLayoutEffect(() => setV(1), [])
+          useLayoutEffect(() => {
+            if (v === 1) props.updatePosition()
+          }, [v])
+
+          return value
+        }
+      }
+      value.type = patchedPopout
+    })
+  }
 }
 
 function patchSelect () {
-  const after = (self, args, value) => {
-    value.props.align = Position.Center
-  }
+  const patcher = createPatcher()
 
-  Patcher.after(...Select, after)
+  Patcher.after(...Select, patcher)
   Patcher.after(...SingleSelect, (...[,, value]) => {
-    Patcher.after(value, 'type', after)
+    Patcher.after(value, 'type', patcher)
   })
 
-  patchSearchableSelect()
+  Patcher.after(SearchableSelect, 'render', createPatcher())
 }
 
 export default patchSelect
