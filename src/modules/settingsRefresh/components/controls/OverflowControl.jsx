3afdfb06b00126d728587a3ccdd69b3152b0b668
Oleg Kapranov <arg0nny@mail.ru>
2025-04-03 00:50:02 +0300

    feat: overflow control reset

--- src/modules/settingsRefresh/components/controls/OverflowControl.jsx
+++ src/modules/settingsRefresh/components/controls/OverflowControl.jsx
@@ -1,24 +1,49 @@
 import { Common, Tooltip } from '@/modules/DiscordModules'
+import IconButton from '@/modules/settingsRefresh/components/IconButton'
+import RedoIcon from '@/modules/settingsRefresh/components/icons/RedoIcon'
+import { css } from '@/modules/Style'
 
-function OverflowControl ({ value, onChange, forced = false }) {
-  const control = (
-    <Common.Checkbox
-      value={value}
-      onChange={(_, value) => onChange(value)}
-      type={Common.Checkbox.Types.INVERTED}
-      disabled={forced}
-    >
-      <Common.Text variant="text-sm/normal">Enable overflow</Common.Text>
-    </Common.Checkbox>
+function OverflowControl ({ value, onChange, forced = false, onReset }) {
+  const control = props => (
+    <div {...props}>
+      <Common.Checkbox
+        value={value}
+        onChange={(_, value) => onChange(value)}
+        type={Common.Checkbox.Types.INVERTED}
+        disabled={forced}
+      >
+        <Common.Text variant="text-sm/normal">Enable overflow</Common.Text>
+      </Common.Checkbox>
+    </div>
   )
 
-  if (!forced) return control
+  const text = () => <>Force {value ? 'enabled' : 'disabled'} by&nbsp;the&nbsp;animation</>
 
   return (
-    <Tooltip text="Value forced by animation" align="left" hideOnClick={false}>
-      {props => <div {...props}>{control}</div>}
-    </Tooltip>
+    <div className="BA__overflowControl">
+      {forced ? (
+        <Tooltip text={text} hideOnClick={false}>
+          {control}
+        </Tooltip>
+      ) : control()}
+      {!forced && onReset && (
+        <IconButton
+          tooltip="Reset"
+          onClick={onReset}
+        >
+          <RedoIcon size="xs" color="currentColor" />
+        </IconButton>
+      )}
+    </div>
   )
 }
 
 export default OverflowControl
+
+css
+`.BA__overflowControl {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}`
+`OverflowControl`
