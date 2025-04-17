e525b1500d498d4084720c549e012c767436f68d
Oleg Kapranov <arg0nny@mail.ru>
2025-04-17 22:26:31 +0300

    fix(snippets): skipped initialize stage

--- src/modules/animation/schemas/injects/snippet.js
+++ src/modules/animation/schemas/injects/snippet.js
@@ -2,6 +2,7 @@ import { z } from 'zod'
 import { InjectSchema, InjectWithMeta } from '@/modules/animation/schemas/utils'
 import Inject from '@/enums/Inject'
 import InjectableSchema from '@/modules/animation/schemas/InjectableSchema'
+import ParseStage from '@/enums/ParseStage'
 
 export const SnippetInjectSchema = (context, env) => InjectSchema(Inject.Snippet).extend({
   key: z.enum(context.pack.snippets?.map(s => s.key) ?? []),
@@ -18,6 +19,11 @@ export const SnippetInjectSchema = (context, env) => InjectSchema(Inject.Snippet
   const snippet = context.pack.snippets?.find(s => s.key === key)
   if (!snippet) return
 
+  const value = InjectableSchema(context, {
+    ...env,
+    stage: ParseStage.Initialize
+  }).parse(snippet.value, { path: ctx.path })
+
   return InjectableSchema(context, {
     ...env,
     snippet: {
@@ -25,7 +31,7 @@ export const SnippetInjectSchema = (context, env) => InjectSchema(Inject.Snippet
       params: Object.assign({}, snippet.params, params),
       depth: env.snippet ? env.snippet.depth + 1 : 1
     }
-  }).parse(snippet.value, { path: ctx.path })
+  }).parse(value, { path: ctx.path })
 })
 
 export const SnippetParamsInjectSchema = InjectWithMeta(
