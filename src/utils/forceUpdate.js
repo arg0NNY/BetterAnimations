ce193b83a1c34dd34d8727f0f7952875d3a04e87
Oleg Kapranov <arg0nny@mail.ru>
2025-06-10 17:14:59 +0300

    feat: improve force update stability

--- src/utils/forceUpdate.js
+++ src/utils/forceUpdate.js
@@ -2,5 +2,5 @@ import { Dispatcher } from '@discord/modules'
 
 export function forceAppUpdate () {
   Dispatcher.dispatch({ type: 'DOMAIN_MIGRATION_START' })
-  setTimeout(() => Dispatcher.dispatch({ type: 'DOMAIN_MIGRATION_SKIP' }))
+  requestIdleCallback(() => Dispatcher.dispatch({ type: 'DOMAIN_MIGRATION_SKIP' }))
 }
