b60350ef64fffe7c52e84cd2a73fa8bf345c1a7c
Oleg Kapranov <arg0nny@mail.ru>
2025-06-23 22:56:09 +0300

    fix: chat search sidebar patch

--- src/patches/ChatSearchSidebar/patchChatSearchSidebar.js
+++ src/patches/ChatSearchSidebar/patchChatSearchSidebar.js
@@ -1,22 +1,23 @@
 import Patcher from '@/modules/Patcher'
 import { ChatSearchSidebarKeyed } from '@discord/modules'
-import findInReactTree from '@/utils/findInReactTree'
+import findInReactTree, { byClassName } from '@/utils/findInReactTree'
 import ensureOnce from '@utils/ensureOnce'
 
-const once = ensureOnce()
-
 function patchChatSearchSidebar () {
-  let cachedScrolls = new Map()
+  const once = ensureOnce()
+  const cachedScrolls = new Map()
 
   Patcher.after(...ChatSearchSidebarKeyed, (self, args, value) => {
-    once(() => {
-      Patcher.after(value.type.prototype, 'componentDidMount', (self) => {
-        self.scrollerRef.current?.scrollTo({ to: cachedScrolls.get(self.props.searchAnalyticsId) ?? 0 })
-      })
-      Patcher.after(value.type.prototype, 'render', (self, args, value) => {
-        const scroller = findInReactTree(value, e => e?.className?.includes('scroller'))
-        if (scroller) scroller.onScroll =
-          () => cachedScrolls.set(self.props.searchAnalyticsId, self.scrollerRef.current?.getDistanceFromTop() ?? 0)
+    Patcher.after(value, 'type', (self, args, value) => {
+      once(() => {
+        Patcher.after(value.type.prototype, 'componentDidMount', (self) => {
+          self.scrollerRef.current?.scrollTo({ to: cachedScrolls.get(self.props.searchAnalyticsId) ?? 0 })
+        })
+        Patcher.after(value.type.prototype, 'render', (self, args, value) => {
+          const scroller = findInReactTree(value, byClassName('scroller'))
+          if (scroller) scroller.props.onScroll =
+            () => cachedScrolls.set(self.props.searchAnalyticsId, self.scrollerRef.current?.getDistanceFromTop() ?? 0)
+        })
       })
     })
   })
