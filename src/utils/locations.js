c7dceb4968d19b3b3d5276a32ff8f9da67c36bea
Oleg Kapranov <arg0nny@mail.ru>
2025-08-19 15:44:27 +0300

    feat: update keep up-to-date parts

--- src/utils/locations.js
+++ src/utils/locations.js
@@ -33,7 +33,6 @@ export function shouldSwitchContent (next, prev) {
     || nextOrPrev(l => l.pathname.startsWith(Routes.GUILD_MEMBER_VERIFICATION_FOR_HUB('')))
     || nextOrPrev(l => matchExact(l.pathname, Routes.GUILD_BOOSTING_MARKETING(GuildChannelRouteParams.guildId())))
     || nextOrPrev(l => matchExact(l.pathname, Routes.COLLECTIBLES_SHOP_FULLSCREEN))
-    || nextOrPrev(l => matchExact(l.pathname, Routes.GUILD_DISCOVERY))
     || nextOrPrev(l => l?.params?.channelId === StaticChannelRoute.GUILD_ONBOARDING, nextChannel, prevChannel)
   ) return true
 
