b049fad33a870d18b34bbb1bacafebfe0b116bf4
Oleg Kapranov <arg0nny@mail.ru>
2025-08-19 16:05:06 +0300

    feat: remove unused modules

--- shared/messages.js
+++ shared/messages.js
@@ -2,7 +2,6 @@
 const Messages = {
   SELECT_ANIMATION_TO_ENABLE: 'Select animation to enable',
   IMPLEMENTED_BY_ANIMATION: animationName => `Implemented by selected animation: ${animationName}`,
-  HEAVY_MODULE_ALERT: moduleName => `${moduleName} animations are heavy on performance, may stutter and slow down app navigation.`,
   SHOULD_BE_VALID_URL: 'Should be a valid URL',
   PRIORITIZE_ANIMATION_SMOOTHNESS: 'Prioritize Animation Smoothness',
   CACHE_USER_SETTINGS_SECTIONS: 'Cache User Settings Sections',
