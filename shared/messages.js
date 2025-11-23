3cda840da4bcf6489111978f8acdf984686093f4
Oleg Kapranov <arg0nny@mail.ru>
2025-11-23 20:34:43 +0300

    feat: update legacy user settings labels

--- shared/messages.js
+++ shared/messages.js
@@ -4,7 +4,7 @@ const Messages = {
   IMPLEMENTED_BY_ANIMATION: animationName => `Implemented by selected animation: ${animationName}`,
   SHOULD_BE_VALID_URL: 'Should be a valid URL',
   PRIORITIZE_ANIMATION_SMOOTHNESS: 'Prioritize Animation Smoothness',
-  CACHE_USER_SETTINGS_SECTIONS: 'Cache User Settings Sections',
+  CACHE_USER_SETTINGS_SECTIONS: 'Cache Legacy User Settings Sections',
   CATALOG_OUT_OF_DATE: 'The last attempt to load the Catalog was unsuccessful. The information below may be out of date.',
   SETTINGS_MIGRATOR: 'Settings Migrator'
 }
