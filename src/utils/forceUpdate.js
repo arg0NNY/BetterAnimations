6bfad733fcbfe9bcfbe33da524cf1a040801b102
Oleg Kapranov <arg0nny@mail.ru>
2026-05-04 19:47:40 +0300

    feat: use domain migration force update

--- src/utils/forceUpdate.js
+++ src/utils/forceUpdate.js
@@ -1,16 +1,6 @@
+import { Dispatcher } from '@discord/modules'
 
 export function forceAppUpdate () {
-  const appMount = document.getElementById('app-mount')
-  const reactContainerKey = Object.keys(appMount).find((m) => m.startsWith('__reactContainer$'))
-  let container = appMount[reactContainerKey]
-  while (!container.stateNode?.isReactComponent) {
-    container = container.child
-  }
-  const { render } = container.stateNode
-  if (render.toString().includes('null')) return
-  container.stateNode.render = () => null
-  container.stateNode.forceUpdate(() => {
-    container.stateNode.render = render
-    container.stateNode.forceUpdate()
-  })
+  Dispatcher.dispatch({ type: 'DOMAIN_MIGRATION_START' })
+  requestIdleCallback(() => Dispatcher.dispatch({ type: 'DOMAIN_MIGRATION_SKIP' }))
 }
