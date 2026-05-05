a13ff12859b8f5bc047fb80df0a375ed889b8c42
Oleg Kapranov <arg0nny@mail.ru>
2026-05-05 15:03:47 +0300

    feat(selectors): fallback to wildcard

--- src/discord/selectors.js
+++ src/discord/selectors.js
@@ -3,9 +3,10 @@ import DiscordClasses from '@discord/classes'
 const DiscordSelectors = new Proxy(DiscordClasses, {
   get (obj, prop) {
     const module = obj[prop]
-    return module && new Proxy({}, {
+    return new Proxy({}, {
       get (_, prop) {
-        return module[prop]?.split(' ').filter(i => !!i).map(c => '.'+c).join('')
+        return module?.[prop]?.split(' ').filter(i => !!i).map(c => '.'+c).join('')
+          ?? `[class^="${prop}_"]`
       }
     })
   }
